c20a8343e326d418e45a67461ed47d35a73ffa03
Meesum Panjwani <meesumusa@hotmail.com>
2021-08-21 17:59:28 -0400

    starting from the beginning, with notes website and will modify

---
+++ README.md
@@ -0,0 +1,10 @@
+# Flask Notes Website
+# The pycache contains the modules for the project
+# The Website file contains all of the frontend and backend for the website
+# The main.py file contains the part of the code that references the rest and actually runs
+# the templates are the frontend HTML and CSS code (uses Bootstrap) of the different login pages
+# the static folder is all of the elements that are static, such as images and other items that do not change (not dynamic)
+# the init dunder file is the part of the code that completes and executes all of the backend parts of the website
+# The views file contains the routing for the home page
+# The auth.py file contains the routing for the login, logout, and sign up pages of the website; it also contains some of the backend that helps with GET and POST requests with HTTP
+# the models.py file contains all of the databases needed, in this case, the user database
--- Website/views.py
+++ Website/views.py
@@ -1,16 +1,37 @@
-from flask import Blueprint
-from flask.templating import render_template
+from Website.models import Note
+from Website.auth import login
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_user, login_required, logout_user, current_user
+from .models import Note, db
+import json
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-@views.route('/')
-def home_page():
-    return render_template('home.html')
+@views.route('/', methods=["GET", "POST"])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
 
-@views.route('/mylyrics')
-def mylyrics_page():
-    return render_template('mylyrics.html')
+        if len(note) < 1:
+            flash("Note is too short", category="error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note Added!", category="success")
+        
 
-@views.route('/alllyrics')
-def alllyrics_page():
-    return render_template('alllyrics.html')
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
